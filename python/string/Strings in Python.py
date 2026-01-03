21a12f5a692e24901fbe8f1db9c838b15fd404e7
Annu-ux-hash <kumawatannu736@gmail.com>
2026-01-03 23:41:24 +0530

    f.string

--- python/string/Strings in Python.py
+++ python/string/Strings in Python.py
@@ -2,10 +2,10 @@
 #string: sequence of character enclosed either in "" or '' or """ """/''' '''
 #" " single line string 
 # ''' multiline string '''
-name = "Anand" #single line 
-print(name)
-introduction = """ my name is anand 
- i am student of bca 1st year 
- i am very  enthusiastic to learn and grow 
-""" 
-print(introduction)
+# name = "Anand" #single line 
+# print(name)
+# introduction = """ my name is anand 
+#  i am student of bca 1st year 
+#  i am very  enthusiastic to learn and grow 
+ """ 
+print(introduction)b
